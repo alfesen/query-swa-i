e7131ae9a81762a4222995e39dd372359ba98ad5
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-05 13:07:45 +0200

    Add basic slideUp keyframes

---
+++ src/css/keyframes.ts
@@ -0,0 +1,12 @@
+import { keyframes } from "@emotion/react";
+
+export const slideUp = keyframes({
+  from: {
+    translate: '0 40%',
+    opacity: '0',
+  },
+  to: {
+    translate: '0 0',
+    opacity: '1',
+  },
+})
