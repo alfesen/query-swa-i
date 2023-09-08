3ee2aab5063521e11c53759719b8e4f9dd9aee50
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-08 12:35:07 +0200

    Create People page

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,43 @@
+import { Global, css } from '@emotion/react'
+import { Navigate, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Fragment } from 'react'
+import People from './pages/People'
+
+const globalStyles = css({
+  '*, *::after, *::before': {
+    margin: 0,
+    padding: 0,
+    boxSizing: 'border-box',
+  },
+  html: {
+    fontSize: '62.5%',
+  },
+  body: {
+    fontFamily: '"Barlow", sans-serif',
+    '.overlay': {
+      zIndex: '200',
+    },
+  },
+  'h1, h2, h3, h4': {
+    fontWeight: 400,
+  },
+  ul: {
+    listStyle: 'none',
+  },
+})
+
 const App = () => {
-  return <></>
+  const routes = createBrowserRouter([
+    { path: '/', element: <Navigate replace to='/people' /> },
+    { path: '/people', element: <People /> },
+  ])
+
+  return (
+    <Fragment>
+      <Global styles={globalStyles} />
+      <RouterProvider router={routes} />
+    </Fragment>
+  )
 }
 
 export default App
