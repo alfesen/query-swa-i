3037502a1e33bf6330bc398bdf4db9059ec37eb7
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-11 13:27:00 +0200

    Complete Stats for planets

--- src/components/Info/Stats.tsx
+++ src/components/Info/Stats.tsx
@@ -1,11 +1,39 @@
+import { Fragment } from 'react'
 import { TStats } from '../../types/typography'
 import Paragraph from '../typography/Paragraph'
+import Heading from '../typography/Heading'
 
 const Stats = (props: TStats) => {
-  if (props.type === 'person') {
+  const { type } = props
+  if (type === 'person') {
     const { height, mass, birth_year } = props.person
     return (
       <Paragraph>{`H: ${height}cm | M: ${mass}kg | BY: ${birth_year}`}</Paragraph>
+    )
+  }
+  if (type === 'planet') {
+    const {
+      rotation_period,
+      orbital_period,
+      diameter,
+      climate,
+      gravity,
+      terrain,
+      surface_water,
+      population,
+    } = props.planet
+    return (
+      <Fragment>
+        <Heading semantic='h4' text={`Terrain: ${terrain}`} />
+        <Paragraph>
+          Time: RP: {rotation_period} hours | OP: {orbital_period} days
+        </Paragraph>
+        <Paragraph>Dimensions: Dia: {diameter}km</Paragraph>
+        <Paragraph>
+          Climate: {climate} | SWL: {surface_water}% | G: {gravity}G
+        </Paragraph>
+        <Paragraph>Population: {population}</Paragraph>
+      </Fragment>
     )
   }
 }
