4c05304c92bfe02f9074d1f19a52fd42ed529151
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-11 08:26:44 +0200

    Add PlanetList to Planets page

--- src/pages/Planets.tsx
+++ src/pages/Planets.tsx
@@ -1,7 +1,12 @@
 import { Fragment } from 'react'
+import PlanetList from '../components/Units/Planets/PlanetList'
 
 const Planets = () => {
-  return <Fragment></Fragment>
+  return (
+    <Fragment>
+      <PlanetList />
+    </Fragment>
+  )
 }
 
 export default Planets
