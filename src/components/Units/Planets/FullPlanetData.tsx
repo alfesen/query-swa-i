0e243e22f0c4b6f7f9f60743eab22233f647ee7c
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-15 19:02:56 +0200

    Create FullPlanetData component for Modal

---
+++ src/components/Units/Planets/FullPlanetData.tsx
@@ -0,0 +1,82 @@
+import styled from '@emotion/styled'
+import { TPlanet } from '../../../types/units'
+import { QueryObserverSuccessResult } from 'react-query'
+import LoadingSpinner from '../../UI/LoadingSpinner'
+import Heading from '../../typography/Heading'
+import { css } from '@emotion/react'
+import Stats from '../../Info/Stats'
+import { useFetch } from '../../../hooks/useFetch'
+import { Fragment } from 'react'
+import LinkGroup from '../../Links/LinkGroup'
+import useUnitId from '../../../hooks/useUnitId'
+
+const planetDataStyles = css({
+  position: 'relative',
+  padding: '2rem',
+  minHeight: '10rem',
+})
+
+const FullPlanetData = ({ id }: { id: number }) => {
+  const { separateId } = useUnitId()
+
+  const {
+    data: planet,
+    error,
+    isLoading,
+    isFetched,
+  } = useFetch(
+    'planets',
+    `planet ${id}`,
+    id
+  ) as QueryObserverSuccessResult<TPlanet>
+
+  const PlanetData = styled.article(planetDataStyles)
+
+  const {
+    climate,
+    diameter,
+    gravity,
+    name,
+    orbital_period,
+    population,
+    rotation_period,
+    surface_water,
+    residents,
+    terrain,
+    films,
+  } = planet || {}
+
+  return (
+    <PlanetData>
+      {isLoading && <LoadingSpinner color='#333' />}
+      {!isLoading && !error && isFetched && (
+        <Fragment>
+          <Heading semantic='h3' text={name} />
+          <Stats
+            type='planet'
+            planet={{
+              climate,
+              diameter,
+              gravity,
+              id,
+              name,
+              orbital_period,
+              population,
+              rotation_period,
+              surface_water,
+              terrain,
+            }}
+          />
+
+          <LinkGroup
+            type='residents'
+            residents={separateId(residents) as string[]}
+          />
+          {<LinkGroup type='films' films={separateId(films) as string[]} />}
+        </Fragment>
+      )}
+    </PlanetData>
+  )
+}
+
+export default FullPlanetData
