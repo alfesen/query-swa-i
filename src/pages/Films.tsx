f9dd4b57e77ebb8664aa6f280df3673b607eb841
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-23 21:22:03 +0200

    Create Films page + route

---
+++ src/pages/Films.tsx
@@ -0,0 +1,12 @@
+import FilmList from '../components/Units/Films/FilmList'
+import { Fragment } from 'react'
+
+const Films = () => {
+  return (
+    <Fragment>
+      <FilmList />
+    </Fragment>
+  )
+}
+
+export default Films
