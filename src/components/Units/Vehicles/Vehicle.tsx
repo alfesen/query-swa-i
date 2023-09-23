23d8082b5ea7a89715166d8f4d1b1a03932f81d4
alfesen <89510380+alfesen@users.noreply.github.com>
2023-09-23 19:35:27 +0200

    Implement Unit logic in Vehicle

--- src/components/Units/Vehicles/Vehicle.tsx
+++ src/components/Units/Vehicles/Vehicle.tsx
@@ -1,9 +1,24 @@
+import { useSearchParams } from 'react-router-dom'
 import { TVehicle } from '../../../types/units'
+import Stats from '../../Info/Stats'
 import ListItem from '../../List/ListItem'
+import Heading from '../../typography/Heading'
 
 const Vehicle = ({ vehicleData }: { vehicleData: TVehicle }) => {
-  const { name } = vehicleData || {}
-  return <ListItem>{name}</ListItem>
+  const [, setSearchParams] = useSearchParams()
+
+  const { name, vehicle_class, id } = vehicleData || {}
+
+  const showFullVehicleData = () => {
+    setSearchParams(`vehicle=${id}`)
+  }
+
+  return (
+    <ListItem onClick={showFullVehicleData}>
+      <Heading semantic='h2' text={`${name} - ${vehicle_class}`} />
+      <Stats type='vehicle' vehicle={vehicleData} />
+    </ListItem>
+  )
 }
 
 export default Vehicle
