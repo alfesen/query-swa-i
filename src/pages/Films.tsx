6cbbf3e9faf3b54369e21d9c012ab32b3457158d
alfesen <89510380+alfesen@users.noreply.github.com>
2023-10-19 11:26:53 +0200

    docker-compose + head titles

--- src/pages/Films.tsx
+++ src/pages/Films.tsx
@@ -1,9 +1,11 @@
+import Head from '@/components/UI/Head'
 import FilmList from '@Units/Films/FilmList'
 import { Fragment } from 'react'
 
 const Films = () => {
   return (
     <Fragment>
+      <Head title='Films' />
       <FilmList />
     </Fragment>
   )
--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,9 +1,11 @@
+import Head from '@/components/UI/Head'
 import SearchInput from '@/components/UI/SearchInput'
 import SearchResult from '@/components/UI/SearchResult'
 
 const Search = () => {
   return (
     <>
+      <Head title='Search' />
       <SearchInput />
       <SearchResult />
     </>
